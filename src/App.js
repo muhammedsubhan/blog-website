4f6eccccfa190064d716f05faea228493bcb7074
Subhan <muhammedsubhan692@gmail.com>
2023-05-29 20:54:54 +0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/createpost">Create Post</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/createpost" element={<CreatePost />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
