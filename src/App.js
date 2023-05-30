8ddc35192e088722bd3017fe91027f0a234fdc2d
Subhan <muhammedsubhan692@gmail.com>
2023-05-30 18:14:56 +0500

    auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Login from "./pages/Login";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
+
 const App = () => {
+  const [isAuth, setIsAuth] = useState(false);
+
+  const navigate = useNavigate();
+
+  const logoutUser = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      navigate("/login");
+    });
+  };
+
   return (
     <>
       <div>
-        <nav>
+      <nav>
           <Link to="/">Home</Link>
           <Link to="/createpost">Create Post</Link>
-          <Link to="/login">Login</Link>
+          {!isAuth ? (
+            <Link to="/login">Login</Link>
+          ) : (
+            <button onClick={logoutUser}>Log Out</button>
+          )}
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/createpost" element={<CreatePost />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
         </Routes>
       </div>
     </>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+
+  // Auth or Sign Button
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
+
+  return (
+    <>
+      <div className="loginPage">
+        <p>Sign In With Google To Continue</p>
+        <button className="login-with-google-btn" onClick={signInWithGoogle}>
+          Sign In With Google
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Login;
